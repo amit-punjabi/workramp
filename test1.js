25e512f1d78fa39d1bef00325d481c50d785620d
Amit Punjabi <amitpunjabi@Amits-MacBook-Pro.local>
2018-09-14 12:01:17 +0530

    Selenium Test 1 updated

--- test1.js
+++ test1.js
@@ -5,13 +5,18 @@ var webdriver = require('selenium-webdriver'),
 var driver = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
-
+//Opens facebook.com
 driver.get('https://www.facebook.com');
+//Then navigates to browserstack.com
 driver.get('https://www.browserstack.com');
+//Use selenium command to simulate 'back' button
 driver.navigate().back();
-//Page should go forward
+//Use selenium command to simulate 'forward' button
 driver.navigate().forward();
+//Use selenium command to simulate 'back' button
 driver.navigate().back();
+
+//Verify if current page title is 'Facebook – log in or sign up'
 driver.getTitle().then(function (websiteTitle) {
     console.log(websiteTitle);
 });
@@ -21,16 +26,15 @@ driver.wait(until.titleIs('Facebook – log in or sign up'), 1000)
     });
 
 
+
+//Print Current URL of page
+driver.getCurrentUrl().then(url => {console.log('current url: "' + url + '"');});
+
+
+//Verify if current URL is 'https://www.facebook.com/'
 driver.wait(until.urlIs('https://www.facebook.com/'), 1000)
     .then(function () {
         console.log('URL is: https://www.facebook.com/');
     });
-
-//Print Current URL of page
-
-driver.getCurrentUrl().then(url => {console.log('current url: "' + url + '"');});
-
-
-//If current URL is https://www.facebook.com/ then print -> URL is: https://www.facebook.com/
-
+    
 driver.quit();
