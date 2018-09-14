1de0981638a2d8379aa410eb782a3a853ecc318f
Amit Punjabi <amitpunjabi@Amits-MacBook-Pro.local>
2018-09-14 12:12:59 +0530

    Browserstack selenium test

---
+++ browserstack_test.js
@@ -0,0 +1,26 @@
+var webdriver = require('selenium-webdriver');
+
+// Input capabilities
+var capabilities = {
+ 'browserName' : 'iPhone',
+ 'device' : 'iPhone 7',
+ 'realMobile' : 'true',
+ 'os_version' : '10.3',
+ 'browserstack.user' : 'amitpunjabi2',
+ 'browserstack.key' : '9GNx3ddF2zRF39bsy6py'
+}
+
+var driver = new webdriver.Builder().
+  usingServer('http://hub-cloud.browserstack.com/wd/hub').
+  usingWebDriverProxy("http://127.0.0.1:8118").
+  withCapabilities(capabilities).
+  build();
+
+driver.get('http://www.google.com').then(function(){
+  driver.findElement(webdriver.By.name('q')).sendKeys('BrowserStack\n').then(function(){
+    driver.getTitle().then(function(title) {
+      console.log(title);
+      driver.quit();
+    });
+  });
+});
