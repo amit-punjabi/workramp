b509086745db451099bdcb7c6c91d52afa816c6f
Amit Punjabi <amitpunjabi@Amits-MacBook-Pro.local>
2018-09-25 13:03:17 +0530

    Proxy with jar

---
+++ proxy_with_jar.js
@@ -0,0 +1,7 @@
+var webdriver = require('selenium-webdriver');
+var capabilities = { 'browserName': 'firefox' }
+
+var driver = new webdriver.Builder().usingServer("http://localhost:4444/wd/hub").withCapabilities(capabilities).usingWebDriverProxy("http://10.100.100.226:8118").build();
+
+driver.get('https://www.google.com');
+    driver.quit();
