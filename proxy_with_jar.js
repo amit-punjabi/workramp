15da6732ca671d1e9224401b98c3a535ebd92d1a
Amit Punjabi <amitpunjabi@Amits-MacBook-Pro.local>
2018-09-25 13:22:18 +0530

    Proxy with jar

--- proxy_with_jar.js
+++ proxy_with_jar.js
@@ -1,4 +1,7 @@
 var webdriver = require('selenium-webdriver');
+
+proxy = require('selenium-webdriver/proxy');
+
 var capabilities = { 'browserName': 'firefox' }
 
 var driver = new webdriver.Builder().usingServer("http://localhost:4444/wd/hub").withCapabilities(capabilities).usingWebDriverProxy("http://10.100.100.226:8118").build();
