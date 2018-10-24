b22ba335bee7dd886da629e65520eab018926d7e
amit-punjabi <amitpunjabi17@gmail.com>
2018-10-24 14:40:22 +0530

    Update test5.js

--- test5.js
+++ test5.js
@@ -15,10 +15,6 @@ driver.get('https://the-internet.herokuapp.com/dynamic_controls');
 
 driver.findElement(By.xpath('//*[@id="checkbox-example"]/button')).click();
 // Verify if text 'It's gone!' exist
-//driver.sleep(5000);
-//function function1(){
-//WebDriverWait wait = new WebDriverWait(driver, 15);
-//wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("message")));
 driver.sleep(5000);
 driver.findElement(By.id('message')).getText().then(function(answer1){
 if(assert.equal("It's gone!",answer1)==undefined){
@@ -28,15 +24,12 @@ else{
     console.log("Fail");
 }});
 driver.sleep(2000);
-//}
-
-//setTimeout(function1, 8000);
 
 // Click the 'Add' button
 driver.findElement(By.xpath('//*[@id="checkbox-example"]/button')).click();
+
 // Verify if text 'It's back!' exist
-//driver.sleep(7000);
-//function function2(){
+
 driver.sleep(10000);
 driver.findElement(By.id('message')).getText().then(function(answer2){
 if(assert.equal("It's back!",answer2)==undefined){
@@ -45,7 +38,7 @@ if(assert.equal("It's back!",answer2)==undefined){
 else{
     console.log("Fail");
 }});
-//}
+
 driver.sleep(2000);
-//setTimeout(function2,16000);
+
 driver.quit();
